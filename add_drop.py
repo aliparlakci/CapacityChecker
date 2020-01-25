48ccc53bb2c70a1e5ab1ab3d4a0a7d37840813ab
Ali Parlakci <parlakciali@gmail.com>
2020-01-25 17:46:31 +0300

    bs4 added

--- add_drop.py
+++ add_drop.py
@@ -1,40 +1,77 @@
 import requests
 import urllib.request
+from bs4 import BeautifulSoup
 from time import sleep
 import pprint
 
-def notification(message,message2):
+def getApiKey(config_file):
+    apiKey = ""
+    with open(config_file,"r") as f:
+        apiKey = f.readlines()[0]
+    return apiKey
+
+def sendNotification(apiKey,message1,message2):
     report = {}
-    report["value1"] = message
+    report["value1"] = message1
     report["value2"] = message2
-    requests.post("https://maker.ifttt.com/trigger/seat_available/with/key/bN2gQNggsfGWraE1KKfbtp", data=report)
+    requests.post("https://maker.ifttt.com/trigger/seat_available/with/key/"+apiKey, data=report)
 
-availability = {}
+def getCourseHtml(crn):
+    course_path = "https://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=201902&crn_in="
+    return urllib.request.urlopen(course_path+crn).read().decode()
 
-crn_codes = []
-crn_codes.append(input("CRN >> "))
+def getAvailability(soup):
+    return int(soup.find_all("td",{"class": "dddefault"})[3].string)
 
-while not crn_codes[-1] == "":
+def getCourseInfo(soup):
+    name, crn, course_code, section = [el.strip() for el in next(soup.find_all("th", {"class":"ddlabel"})[0].strings).split("-")]
+    return {
+        "name": name,
+        "crn": crn,
+        "code": course_code,
+        "section": section
+    }
+
+def getCrnCodes():
+    crn_codes = []
     crn_codes.append(input("CRN >> "))
-crn_codes = crn_codes[:-1]
 
-print()
-    
-course_site = "http://suis.sabanciuniv.edu/prod/bwckschd.p_disp_detail_sched?term_in=201901&crn_in="
+    while not crn_codes[-1] == "":
+        crn_codes.append(input("CRN >> "))
+    print()
+    return crn_codes[:-1]
+
+def main():
+    prevAvailability = {}
+    crn_codes = getCrnCodes()
+    apiKey = getApiKey("api_key.txt")
+    while True:
+        for crn in crn_codes:
+            soup = BeautifulSoup(getCourseHtml(crn), 'html.parser')
+            course_info = getCourseInfo(soup)
+            course_info["availability"] = getAvailability(soup)
+
+            print(f"{crn} - {course_info['code']} {course_info['section']} >> {course_info['availability']}",end="")
+
+            # TODO
+            # Bug: Program doesn't send notification
+            # if there are available seats on the first check
+
+            if not crn in prevAvailability:
+                prevAvailability[crn] = course_info["availability"]
+
+            if (course_info["availability"] != 0) and (not prevAvailability[crn] == course_info["availability"]):
+                title = f"{crn} - {course_info['code']} {course_info['section']}"
+                details = f"{course_info['availability']} seat available."
+                sendNotification(apiKey, title, details)
+                prevAvailability[crn] = course_info["availability"]
+                print(", notification sent!")
+            
+            print()
+
+        sleep(1)
 
 try:
-    while True:
-        for i in range(len(crn_codes)):
-            pageSource = (urllib.request.urlopen(course_site+crn_codes[i]).read().decode())
-            course_name = "-".join(pageSource.split("\n")[119].split(">")[1].split("<")[0].split("-")[-2:]) + " - " + crn_codes[i]
-            try:
-                if not availability[crn_codes[i]] == pageSource.split("\n")[148].split(">")[1].split("<")[0]:
-                    availability[crn_codes[i]] = pageSource.split("\n")[148].split(">")[1].split("<")[0]
-                    notification(crn_codes[i], course_name)
-            except KeyError:
-                availability[crn_codes[i]] = pageSource.split("\n")[148].split(">")[1].split("<")[0]
-            print(course_name + " >> " + availability[crn_codes[i]])
-        print()
-        sleep(1)
+    main()
 except KeyboardInterrupt:
     quit()
